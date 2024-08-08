ca16f64c31a0cddaa026e536149cd9e7dc407bc5
mdLafrance <maxlafrance97@gmail.com>
2024-08-08 14:03:48 -0400

    Cleanup explanations

--- src/components/Explanation.tsx
+++ src/components/Explanation.tsx
@@ -5,6 +5,33 @@ import { VersionPrefix } from "@/types";
 import { IconInfoCircle } from "@tabler/icons-react";
 import { useState } from "react";
 
+const tildeExplanation = (
+    <p>
+        A <strong>tilde</strong> constraint is usually used to allow for updates to patch versions.
+        <br />
+        <br />
+        If a minor or patch level version is specified, <strong>only </strong>patch-level changes are allowed.
+        <br />
+        <br />
+        However, if <strong>only </strong>a <strong>major</strong> version is specified, minor-level changes are also accepted.
+        <br />
+        <br />
+        For more information, see <a href="https://www.npmjs.com/package/semver" target="_blank" rel="noreferrer" className="underline decoration-primary-600 text-primary-600">npmjs.com/pakage/semver</a>.
+    </p>
+)
+
+const caretExplanation = (
+    <p>
+        A caret constraint allows changes that do <strong>not </strong>modify the <strong>left-most </strong>non-zero element in the [major, minor, patch] tuple.
+        <br />
+        <br />
+        In other words, this allows patch and minor updates for versions 1.0.0 and above, patch updates for versions 0.X &gt; 0.1.0, and no updates for versions 0.0.X.
+        <br />
+        <br />
+        For more information, see <a href="https://www.npmjs.com/package/semver" target="_blank" rel="noreferrer" className="underline decoration-primary-600 text-primary-600">npmjs.com/pakage/semver</a>.
+    </p>
+)
+
 export default function Explanation({ constraint }: { constraint: VersionPrefix }) {
     const [isExpanded, setIsExpanded] = useState(false);
 
@@ -15,32 +42,9 @@ export default function Explanation({ constraint }: { constraint: VersionPrefix 
     var explanationText: React.ReactNode
 
     if (constraint === "~") {
-        explanationText = (
-            <p>
-                A <strong>tilde</strong> constraint is usually used to allow for updates to patch versions.
-                <br />
-                <br />
-                If a minor or patch level version is specified, <strong>only </strong>patch-level changes are allowed.
-                <br />
-                <br />
-                However, if <strong>only </strong>a <strong>major</strong> version is specified, minor-level changes are also accepted.
-                <br />
-                <br />
-                For more information, see <a href="https://www.npmjs.com/package/semver" target="_blank" rel="noreferrer" className="underline decoration-primary-600 text-primary-600">npmjs.com/pakage/semver</a>.
-            </p>
-        )
+        explanationText = tildeExplanation
     } else if (constraint === "^") {
-        explanationText = (
-            <p>
-                A caret constraint allows changes that do <strong>not </strong>modify the <strong>left-most </strong>non-zero element in the [major, minor, patch] tuple. 
-                <br />
-                <br />
-                In other words, this allows patch and minor updates for versions 1.0.0 and above, patch updates for versions 0.X &gt; 0.1.0, and no updates for versions 0.0.X.
-                <br />
-                <br />
-                For more information, see <a href="https://www.npmjs.com/package/semver" target="_blank" rel="noreferrer" className="underline decoration-primary-600 text-primary-600">npmjs.com/pakage/semver</a>.
-            </p>
-        )
+        explanationText = caretExplanation
     }
 
     return (
